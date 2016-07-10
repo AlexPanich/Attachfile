d6a3946e73994f9fd233c28ad9088ef4def654db
AlexPanich <nasada@yandex.ru>
2016-07-10 07:54:38 +0300

    firs commit

---
+++ src/AttachfileServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AlexPanich\Attachfile;
+
+use Illuminate\Support\ServiceProvider;
+
+class AttachfileServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
